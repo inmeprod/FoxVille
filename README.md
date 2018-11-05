0248ab1fdbf6f990035153af64002ed32551dd2c
INME <inme.prod@gmail.com>
2018-11-06 01:46:54 +0200

    1.0.2
    Clear js

---
+++ README.md
@@ -0,0 +1,7 @@
+# FoxVille
+PWA project without using any 3rd party frameworks like a bootstrap or jQuery.
+
+Development of project that doesn't need some 3rd party frameworks like bootstrap/jQuery.
+
+A grid system based on the flex display property.
+All functions in native js without some vendors.
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,29 +1,9 @@
 /*!
- *
- *  Web Starter Kit
- *  Copyright 2015 Google Inc. All rights reserved.
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *    https://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License
- *
- */
 /* eslint-env browser */
 (function() {
   'use strict';
 
   // Check to make sure service workers are supported in the current browser,
-  // and that the current page is accessed from a secure origin. Using a
-  // service worker from an insecure origin will trigger JS console errors. See
-  // http://www.chromium.org/Home/chromium-security/prefer-secure-origins-for-powerful-new-features
   var isLocalhost = Boolean(window.location.hostname === 'localhost' ||
       // [::1] is the IPv6 localhost address.
       window.location.hostname === '[::1]' ||
@@ -45,7 +25,6 @@
         // i.e. whether there's an existing service worker.
         if (navigator.serviceWorker.controller) {
           // The updatefound event implies that registration.installing is set:
-          // https://slightlyoff.github.io/ServiceWorker/spec/service_worker/index.html#service-worker-container-updatefound-event
           var installingWorker = registration.installing;
 
           installingWorker.onstatechange = function() {
